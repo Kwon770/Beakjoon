8754dda11ea6853001b8df82aa18dac2f4dd1395
Kwon770 <sckwon770@gmail.com>
2020-02-05 15:00:17 +0900

    Backtrack(2) 15649

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+#define MAX 9
+
+int n, m;
+bool visited[MAX];
+vector<int> v;
+
+void PrintAll() {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+void Dfs(int cnt) {
+    if (cnt == m) {
+        PrintAll();
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (visited[i]) continue;
+
+        visited[i] = true;
+        v.push_back(i);
+        Dfs(cnt + 1);
+        visited[i] = false;
+        v.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    Dfs(0);
+    return 0;
+}
