41c433964c5dfe0e14ae67e0f09408756dccea51
Kwon770 <sckwon770@gmail.com>
2020-03-15 19:24:41 +0900

    Dynamic(11) 11053

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
-// https://www.acmicpc.net/problem/2156
-// Dynamic Programming (10), 포도주 시식
-// NUMBER 2156
+// https://www.acmicpc.net/problem/11053
+// Dynamic Programming (11), 가장 긴 증가하는 부분 수열
+// NUMBER 11053
 
 #include <iostream>
 
 using namespace std;
-#define MAX 10001
-int input[MAX], result[MAX];
+#define MAX 1001
+int input[MAX], maxLen = 0;
 
 void Solve(int n)
 {
-    result[1] = input[1];
-    result[2] = input[2] + input[1];
-    for (int i = 3; i <= n; i++)
-        result[i] = max(result[i - 1], max(result[i - 2] + input[i], result[i - 3] + input[i - 1] + input[i]));
-    cout << result[n] << endl;
+    int result[n + 1];
+    fill_n(result, n + 1, 1);
+    for (int i = n; i > 0; i--)
+    {
+        for (int o = i; o <= n; o++)
+        {
+            if (input[i] < input[o])
+                result[i] = max(result[i], result[o] + 1);
+            maxLen = max(maxLen, result[i]);
+        }
+    }
+    cout << maxLen << endl;
 }
 
 int main()
