d6420beb4588e2f694bd3f90354a9a4f7a8434d7
Kwon770 <sckwon770@gmail.com>
2020-03-29 12:57:03 +0900

    Greedy Algorithm(1) 11047

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-// https://www.acmicpc.net/problem/12865
-//
-// NUMBER
+// https://www.acmicpc.net/problem/11047
+// Greedy Algorithm(1), 동전 0
+// NUMBER 11047
 
 #include <iostream>
 using namespace std;
@@ -11,18 +11,5 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, input;
-    cin >> n;
-    while (true)
-    {
-        cin >> input;
-        if (!input)
-            break;
-
-        if (input % n == 0)
-            cout << input << " is a multiple of " << n << ".\n";
-        else
-            cout << input << " is NOT a multiple of " << n << ".\n";
-    }
-    return 0;
+        return 0;
 }
