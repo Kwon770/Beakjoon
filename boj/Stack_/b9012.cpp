9ba6f2a941faff1e9a4ed312f1dbe917e6f338e1
Kwon770 <sckwon770@gmail.com>
2020-04-01 15:51:32 +0900

    Stack(3) 9012

--- boj/Stack_/b9012.cpp
+++ boj/Stack_/b9012.cpp
@@ -2,6 +2,11 @@
 // Stack(3), 괄호
 // NUMBER 9012
 
+// 괄호가 정상적일려면 '(' 과 ')' 이 짝이 맞아야 되므로, ')' 이 나온 앞에 무조건 '(' 이 나와야만 한다는 것을 이용
+// 순서대로 확인하므로 vector 을 이용
+
+// 벡터를 확인할때 올바르지 못한 식일 경우, 백터를 확인할때 런타임 오류가 뜰 수 있으므로, vec.size() 를 확인한다,
+
 #include <iostream>
 #include <vector>
 using namespace std;
