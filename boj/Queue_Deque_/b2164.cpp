ce44f47652d556eab9f7386f5af3f9c0609aa589
Kwon770 <sckwon770@gmail.com>
2020-04-03 15:29:03 +0900

    Queue_Deque(3) 2164

--- boj/Queue_Deque_/b2164.cpp
+++ boj/Queue_Deque_/b2164.cpp
@@ -2,6 +2,12 @@
 // Queue_Deque_(2), 카드 2
 // NUMBER 2164
 
+// Queue/Dequq 를 이용하여 주어진 문제의 흐름대로 조작하는 문제
+// erase 할때마다 인덱스가 한 칸씩 앞으로 당겨지므로 k 만큼 인덱스를 옮길때 조심하여야 함
+// erase 할때마다 최대 인덱스가 변하므로, 인덱스를 증가 시킬때 주의하여야 함
+// => 단지 curIdx - queueSize 만큼 할 경우, 뺸 후에도 최대 인덱스보다 큰 경우가 있을 수 있음
+// deque 에 들어있는 숫자는 1부터 시작이지만, deque 의 시작인덱스는 0임
+
 #include <iostream>
 #include <deque>
 using namespace std;
