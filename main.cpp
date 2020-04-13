67d530e65db516b035a81aa8d44fcc33038d3d55
Kwon770 <sckwon770@gmail.com>
2020-04-13 15:18:20 +0900

    Divide_and_conquer(6) 2740

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-// https://www.acmicpc.net/problem/11401
-// Divide_and_conquer(5), 이항 계수 3
-// NUMBER 11401
+// https://www.acmicpc.net/problem/2740
+// Divide_and_conquer(6), 행렬 곱셈
+// NUMBER 2740
 
 #include <iostream>
 using namespace std;
@@ -12,14 +12,6 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n, k;
-    cin >> n >> k;
-    facMod[0] = 1;
-    facMod[1] = 1;
-    facMod[2] = 2;
-    for (int i = 3; i <= n; i++)
-        facMod[i] = (i * facMod[i - 1]) % MOD;
-    long long second = (facMod[k] * facMod[n - k]) % MOD;
-    cout << (facMod[n] % MOD) * (power(second, MOD - 2) % MOD) % MOD << endl;
+
     return 0;
 }
