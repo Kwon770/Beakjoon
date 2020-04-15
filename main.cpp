0290fff52444b015b7fe30afd43b9ef138691853
Kwon770 <sckwon770@gmail.com>
2020-04-15 17:11:24 +0900

    Divide_and_conquer(8) 2749

--- main.cpp
+++ main.cpp
@@ -1,36 +1,28 @@
-// https://www.acmicpc.net/problem/10830
-// Divide_and_conquer(7), 행렬 제곱
-// NUMBER 10830
+// https://www.acmicpc.net/problem/2749
+// Divide_and_conquer(8), 피보나치 수3
+// NUMBER 2749
 
 #include <iostream>
 #include <vector>
 using namespace std;
 #define endl "\n"
-
+#define MOD 1000000
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    vector<vector<long long>> Result(6, vector<long long>(6, 0));
-    vector<vector<long long>> Origin(6, vector<long long>(6, 0));
-    long long b;
-    cin >> n >> b;
-    for (int i = 1; i <= n; i++)
-        for (int o = 1; o <= n; o++)
-        {
-            cin >> Result[i][o];
-            Origin[i][o] = Result[i][o];
-        }
-
-    if (b > 1)
-        divide(b, Result, Origin);
-
-    for (int i = 1; i <= n; i++)
+    int p = 1500000, fibo[1500000];
+    fibo[0] = 0;
+    fibo[1] = fibo[2] = 1;
+    long long n;
+    cin >> n;
+    n %= p;
+    for (int i = 3; i <= n; i++)
     {
-        for (int o = 1; o <= n; o++)
-            cout << Result[i][o] % MOD << " ";
-        cout << endl;
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+        fibo[i] %= MOD;
     }
+    cout << fibo[n] << endl;
     return 0;
 }
