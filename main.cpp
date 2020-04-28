7b6706baf2afa3c8f27e25fed9789c636c20f507
Kwon770 <sckwon770@gmail.com>
2020-04-28 10:32:40 +0900

    Binaray Search(7) 12015

--- main.cpp
+++ main.cpp
@@ -1,35 +1,16 @@
-// https://www.acmicpc.net/problem/1300
-// Binaray Search(6), K번째 수
-// NUMBER 1300
+// https://www.acmicpc.net/problem/12015
+// Binaray Search(7), 가장 긴 증가하는 부분 수열 2
+// NUMBER 12015
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
-#define endl '\n'
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int a, b, cnt = 0;
-    cin >> a >> b;
-    while (true) {
-        if (b == a) {
-            break;
-        } else if (b < a) {
-            cnt = -2;
-            break;
-        } else {
-            if (b % 2 == 0) {
-                b /= 2;
-            } else if (b % 10 != 1) {
-                cnt = -2;
-                break;
-            } else {
-                b /= 10;
-            }
-            cnt++;
-        }
-    }
-    cout << cnt + 1 << endl;
+
     return 0;
 }
