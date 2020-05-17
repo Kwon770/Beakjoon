b08b91b4ba7ddb14b24a0ec68708c6fe639c1556
Kwon770 <sckwon770@gmail.com>
2020-05-17 16:30:52 +0900

    Dynamic Programming 2 (6) 10942

--- main.cpp
+++ main.cpp
@@ -1,87 +1,37 @@
 // https://www.acmicpc.net/problem/11723
 // NUMBER 11723
 
-#include <algorithm>
+#include <cstring>
 #include <iostream>
 #define endl "\n"
 using namespace std;
+int arr[2001], memo[2001][2001];
 
-class Set {
-    int* arr;
+int pali(int from, int to) {
+    if (from >= to) return 1;
 
-   public:
-    Set() {
-        arr = new int[20];
-        fill(arr, arr + 20, 0);
-    }
-    ~Set() {
-        delete arr;
-    }
+    int &result = memo[from][to];
+    if (result != -1) return result;
 
-    void add(int i) {
-        if (!arr[i - 1]) arr[i - 1] = i;
-    }
+    if (arr[from] != arr[to]) return result = 0;
 
-    void remove(int i) {
-        if (arr[i - 1]) arr[i - 1] = 0;
-    }
-
-    void check(int i) {
-        if (arr[i - 1])
-            cout << 1 << endl;
-        else
-            cout << 0 << endl;
-    }
-
-    void toggle(int i) {
-        if (arr[i - 1])
-            arr[i - 1] = 0;
-        else
-            arr[i - 1] = i;
-    }
-
-    void all() {
-        delete arr;
-        arr = new int[20];
-        for (int i = 0; i < 20; i++) {
-            arr[i] = i + 1;
-        }
-    }
-
-    void empty() {
-        delete arr;
-        arr = new int[20];
-        fill(arr, arr + 20, 0);
-    }
-};
+    return result = pali(from + 1, to - 1);
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    Set set = Set();
-    string cmd;
-    int n, temp;
+    int n, m;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> cmd;
-        if (cmd == "add") {
-            cin >> temp;
-            set.add(temp);
-        } else if (cmd == "remove") {
-            cin >> temp;
-            set.remove(temp);
-        } else if (cmd == "check") {
-            cin >> temp;
-            set.check(temp);
-        } else if (cmd == "toggle") {
-            cin >> temp;
-            set.toggle(temp);
-        } else if (cmd == "all")
-            set.all();
-        else if (cmd == "empty")
-            set.empty();
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &arr[i]);
+    memset(memo, -1, sizeof(memo));
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int f, t;
+        scanf("%d %d", &f, &t);
+        printf("%d\n", pali(f, t));
     }
     return 0;
 }
