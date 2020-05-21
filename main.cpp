b8f8fcc5a39ff4ba983f65b0fd0ebd140ff6ecfe
Kwon770 <sckwon770@gmail.com>
2020-05-21 13:34:30 +0900

    DFS-BFS (3), 2667

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
-// https://www.acmicpc.net/problem/2606
-// DFS-BFS (1), 바이러스
-// NUMBER 2606
+// https://www.acmicpc.net/problem/2667
+// NUMBER 2667
 
+#include <algorithm>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <vector>
 #define endl "\n"
 using namespace std;
 
-int graph[101][101], visited[101], num, connection, cnt = 0;
-
-void dfs(int n) {
-    for (int i = 1; i <= num; i++) {
-        if (graph[n][i] && !visited[i]) {
-            cnt++;
-            visited[i] = 1;
-            dfs(i);
-        }
-    }
-}
-
 int main() {
-    memset(graph, 0, sizeof(graph));
-    memset(visited, 0, sizeof(visited));
-    cin >> num >> connection;
-    int tmp1, tmp2;
-    for (int i = 0; i < connection; i++) {
-        cin >> tmp1 >> tmp2;
-        graph[tmp1][tmp2] = 1;
-        graph[tmp2][tmp1] = 1;
-    }
-    visited[1] = 1;
-    dfs(1);
-    cout << cnt << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     return 0;
 }
