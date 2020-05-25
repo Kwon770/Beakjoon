42b27e5cb4a5bb40ab8e63b60b87509f0e7e8e81
Kwon770 <sckwon770@gmail.com>
2020-05-25 18:22:29 +0900

    b18870

--- boj/DFS-BFS/b1012.cpp
+++ boj/DFS-BFS/b1012.cpp
@@ -3,9 +3,9 @@
 // NUMBER 1012
 
 // DFS BFS, https://www.notion.so/sckwon770/DFS-BFS-6ec22005435048b18669df0f2541538c
-// 2667번, https://github.com/Kwon770/Algorithm/blob/master/boj/DFS-BFS/b2667.cpp
 
 // 2667번 단지번호 붙이기와 거의 비슷한 DFS다.
+// 2667번, https://github.com/Kwon770/Algorithm/blob/master/boj/DFS-BFS/b2667.cpp
 // 입력과 출력, 그리고 주어지는 인덱스가 0부터 시작하며,
 // 각각의 붙어있는 요소의 개수가 필요없으므로 cnt 변수의 쓰임에 약간의 변형이 필요하다.
 
