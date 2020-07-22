f73ee2722a3424863e5c7fe7977de0d905364510
Kwon770 <sckwon770@gmail.com>
2020-07-23 00:37:24 +0900

    java-warming b2577

---
+++ Main.java
@@ -0,0 +1,13 @@
+// 입출력과 사칙연산(1), Hello World
+// https://www.acmicpc.net/problem/2557
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ boj/warming_up/b2557.java
@@ -0,0 +1,14 @@
+// 입출력과 사칙연산(1), Hello World
+// https://www.acmicpc.net/problem/2557
+package boj.warming_up;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class b2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
