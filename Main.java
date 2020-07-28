96583a339b6d4c0cbe1ef6675eca410b2c44afca
Kwon770 <sckwon770@gmail.com>
2020-07-28 13:54:44 +0900

    java-warming b1000

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
-// 입출력과 사칙연산(3), 고양이
-// https://www.acmicpc.net/problem/10171
-
-// 자바는 여러 줄 문자열 리털이 없기때문에 
-// + 로 연결하거나 StringBuilder 등을 이용해 붙여야 한다.
-
-// 이에 대해 자세히 다룬 자료
-// https://it-man.tistory.com/586
+// https://www.acmicpc.net/problem/14681
+// 입출력과 사칙연산(5), A + B
+// NUMBER 14681
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -14,7 +9,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\\    /\\ \n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|");
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        x = scanner.nextInt();
+        y = scanner.nextInt();
 
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
+        } else {
+            if (y > 0) {
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
+
+        }
+        scanner.close();
     }
 }
