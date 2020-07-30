48cdf087ae6119e31e1509a062c5968e116d235c
Kwon770 <sckwon770@gmail.com>
2020-07-30 20:10:42 +0900

    Pratice(5) 별찍기 - 9

--- boj/Pratice/b2446.java
+++ boj/Pratice/b2446.java
@@ -1,16 +1,11 @@
 // https://www.acmicpc.net/problem/2523
-// 실습 (4), 별찍기 - 13
+// 실습 (5), 별찍기 - 9
 // NUMBER 2523
+package boj.Pratice;
 
-// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// Scanner scanner = new Scanner(System.in);
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.Scanner;
 
-public class Main {
+public class b2446 {
     static int n;
     static StringBuilder sb;
 
