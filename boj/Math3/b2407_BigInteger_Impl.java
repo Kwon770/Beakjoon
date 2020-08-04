b042347e4ef68d4a8527e5f90870f1b98fc1fe0a
Kwon770 <sckwon770@gmail.com>
2020-08-04 09:34:30 +0900

    Math: b2407

--- boj/Math3/b2407_BigInteger_Impl.java
+++ boj/Math3/b2407_BigInteger_Impl.java
@@ -1,15 +1,17 @@
 // https://www.acmicpc.net/problem/2407
 // NUMBER 2407, 조합
 
-// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// Scanner scanner = new Scanner(System.in);
+// 단순한 조합 계산 문제지만, 답의 결과범위가 단순하지 않다.
+// 예시로 나온 100C6은 long 범위안이지만, 100C50같은 경우 100891344545564193334812497256 으로 long 범위를 벗어난다.
+// 이러한 큰 범위의 계산은 BigInteger을 사용해야한다.
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+// 이 코드는 BigInteger의 덧셈을 직접 구현하였다.
+
+package boj.Math3;
+
 import java.util.Scanner;
 
-public class Main {
+public class b2407_BigInteger_Impl {
     static String cache[][] = new String[101][101];
 
     public static void main(String[] args) {
@@ -27,6 +29,7 @@ public class Main {
         scanner.close();
     }
 
+    // nCr = n-1Cr + n-1Cr-1
     public static String combination(int n, int r) {
         if (n == r || r == 0)
             return "1";
@@ -42,6 +45,7 @@ public class Main {
         long sum = 0;
         StringBuilder result = new StringBuilder("");
 
+        // 1의 자리부터 더하기 사작한다
         while (!num1.isEmpty() || !num2.isEmpty() || sum != 0) {
             if (!num1.isEmpty()) {
                 sum += num1.charAt(num1.length() - 1) - '0';
@@ -51,10 +55,11 @@ public class Main {
                 sum += num2.charAt(num2.length() - 1) - '0';
                 num2 = num2.substring(0, num2.length() - 1);
             }
-
+            // 다시 String 형태로 만들어 append
             result.append(Long.toString(sum % 10));
             sum /= 10;
         }
+        // 1의 자리부터 result에 넣었으므로 뒤집어야 한다.
         return result.reverse().toString();
     }
 }
