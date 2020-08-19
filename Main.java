466d0d876d5599c22ed935cfee4013e653e7f1f4
Kwon770 <sckwon770@gmail.com>
2020-08-19 13:35:55 +0900

    Floyd-Warshall: b11404.java refactoring

--- Main.java
+++ Main.java
@@ -19,11 +19,12 @@ public class Main {
         int m = Integer.parseInt(br.readLine());
 
         int[][] dist = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                dist[i][j] = i == j ? 0 : Integer.MAX_VALUE;
-            }
-        }
+        for (int[] row : dist)
+            // 불가능한 경우는 int의 최댓값으로 초기화
+            // 단순히 최대 가중치 10만보다 큰 수로는 최대값이 될 수 없다.
+            // 1. 플로이드 와샬 과정 중에 두 개의 노선이 더해짐.
+            // 2. 출력을 위한 마지막 for문에서 모든 노선이 더해지는 경우, 9,900,000 까지도 될 수 있음.
+            Arrays.fill(row, Integer.MAX_VALUE);
 
         for (int i = 1; i <= m; i++) {
             int[] input = parseIntArr(br.readLine().split(" "));
