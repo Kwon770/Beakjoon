96cacdd86fe39dea00fb707288c98e17de14a6b4
Kwon770 <sckwon770@gmail.com>
2020-09-28 11:57:19 +0900

    dp_reverse_tracking: b13913.cpp

--- boj/dp_reverse_tracking/b13913.cpp
+++ boj/dp_reverse_tracking/b13913.cpp
@@ -1,6 +1,16 @@
 // https://www.acmicpc.net/problem/14003
 // DP and Shortest path reverse tracking(6), 13913 숨바꼭질4
 
+// 각 좌표에 fastest time 을 가지고, 현재 확인하는 좌표로 부터의 이동이
+// 이미 계산된 fastest time 보다 더 빠른 경우(가치있는 경우, 갱싱해야 하는 경우)
+// fastest time을 갱신한다.
+
+// 경로 추적이 필요하므로 별도의 previous 배열을 통해 자취를 추적한다.
+// previous 배열을 이용하는 경우 출력할때 뒤집어야 하는데, 이를 해결하기 위해
+// next 배열로 바꾸는 것은 불가능하다. 코드의 흐름상 한 좌표에서 다른 여러 좌표로
+// 갈 수 있는 경우가 많다. 따라서 여러 좌표의 적절성을 판단하는 과정에서 해당 좌표의 next 배열이
+// 계속 갱신되면서 쓰레기값이 들어갈 수 있다.
+
 #include <algorithm>
 #include <cmath>
 #include <cstring>
--- temp.cpp
+++ temp.cpp
@@ -1,31 +1,7 @@
-#include <functional>
-#include <iostream>
-#include <queue>
-#include <vector>
+#include <stdio.h>
 
-using namespace std;
+int main() {
+    printf("%s\n%s", "¾ÆÀÌÄÜ ¸¸¼¼", "¾ÆÀÌÄÜ ¸¸¼¼");
 
-int main(void)
-
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int N;
-    cin >> N;
-    priority_queue<int> pq;
-
-    for (int i = 0; i < N; i++) {
-        int num;
-        cin >> num;
-
-        if (!num && pq.empty())
-            cout << 0 << "\n";
-        else if (!num) {
-            cout << pq.top() << "\n";
-            pq.pop();
-        } else
-            pq.push(num);
-    }
     return 0;
 }
