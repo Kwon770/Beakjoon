394c16844d46d037aaa02c01ec95205fb76a1030
Kwon770 <sckwon770@gmail.com>
2021-03-26 01:44:46 +0900

    codeforce#710 - A B C

--- main.cpp
+++ main.cpp
@@ -1,53 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 #define endl "\n"
 using namespace std;
 
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
+#define For(i, j, k) for (int i = j; i < (int)(k); i++)
 #define ln(x) (int)(x).length()
 #define sz(x) (int)(x).size()
 #define F first
 #define S second
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    int v, e, start;
-    cin >> v >> e >> start;
-    vector<vector<pair<int, int>>> edges(v + 1, vector<pair<int, int>>());
-    vector<int> dist(v + 1, INT_MAX);
-    dist[start] = 0;
-    rep(i, e) {
-        int from, to, w;
-        cin >> from >> to >> w;
-        edges[from].push_back({to, w});
-    }
+  int t;
+  cin >> t;
+  For(i, 0, t) {
+    string str;
+    cin >> str;
 
-    priority_queue<pair<int, int>> pq;
-    pq.push({0, start});
-    while (!pq.empty()) {
-        int cur = pq.top().S;
-        pq.pop();
-
-        for (auto edge : edges[cur]) {
-            int curVal = dist[edge.F];
-            int newVal = dist[cur] + edge.S;
-
-            if (newVal < curVal) {
-                dist[edge.F] = newVal;
-                pq.push({-1 * newVal, edge.F});
-            }
+    int failIdx = ln(str) - 1;
+    int last = str[ln(str) - 1];
+    bool changed = false;
+    for (auto iter = str.rbegin() + 1; iter != str.rend(); iter++) {
+      if (last == '0') {
+        if (*iter == '1') {
+          int tmp = iter - str.rbegin();
+          failIdx = ln(str) - tmp - 1;
+          break;
         }
-    }
+      } else {
+        if (changed) {
+          if (*iter == '1') {
+            int tmp = iter - str.rbegin();
+            failIdx = ln(str) - tmp - 1;
+            break;
+          }
+        } else {
+          if (*iter == '0')
+            changed = true;
+        }
+      }
 
-    rep1(i, v) {
-        if (dist[i] == INT_MAX)
-            cout << "INF";
-        else
-            cout << dist[i];
-        cout << endl;
+      return 0;
     }
-    return 0;
-}
