93f29295a139130ce742c4352388ee6e4be40428
Kwon770 <sckwon770@gmail.com>
2021-08-02 21:26:42 +0900

    chore: Set CLion configuration with gcc@9

--- main.cpp
+++ main.cpp
@@ -1,46 +1,12 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
 
 #define ln(x) (int)(x).length()
 #define sz(x) (int)(x).size()
-#define F first
-#define S second
+#define ll long long
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+int main(void) {
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
-  int n;
-  string inp;
-  cin >> n >> inp;
-
-  int idx = 0;
-  for (int i = 0; i < sz(inp); i++) {
-    if (inp[i] == 'J' || inp[i] == 'A' || inp[i] == 'V') {
-      continue;
-    }
-
-    inp[idx] = inp[i];
-    idx++;
-  }
-
-  if (idx == 0) {
-    cout << "nojava";
     return 0;
-  }
-
-  for (int i = idx; i < sz(inp); i++) {
-    inp[i] = ' ';
-  }
-
-  cout << inp;
-  return 0;
 }
